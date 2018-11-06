9f823f4cf8c78edc85e6142b8b63ef191358dc8c
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2018-11-06 11:32:04 +0700

    add two job for example

--- example.js
+++ example.js
@@ -1,8 +1,19 @@
 const Cronodile = require('./')
-const cron = new Cronodile('id')
 
-cron
-  .command(() => {
-    console.log('run your command here')
-  })
-  .run('setiap menit')
+const jobOne = new Cronodile('id')
+const jobTwo = new Cronodile('id')
+
+jobOne
+	.command(() => {
+		console.log('[JOB1] run your command here')
+	})
+	.run('setiap menit')
+
+jobTwo.command(() => {
+		console.log('[JOB2] run your command here')
+	})
+	.run('setiap menit')
+
+// cron
+//   .fromFile('./example_file')
+//   .run('setiap menit')
